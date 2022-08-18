2d897ee3e585403939d69eff83e4a8081b9a5cf9
Gavin Schulze <gbsch313@learner.dvusd.org>
2022-08-18 11:47:11 -0700

    Updated

---
+++ Java/CodeHS.java
@@ -0,0 +1,13 @@
+
+public class CodeHS
+{
+   static final int max = 8;
+   public static void main(String args[])
+   {
+       int n = 0;
+       double d = 1.2;
+       String s = "Hello";
+       
+       System.out.println(max);
+   }
+}
---
+++ Java/Quiz.java
@@ -0,0 +1,33 @@
+
+
+public class Quiz
+{
+    public static void main(String args[])
+    
+    {
+        System.out.println("IS Kobe the Goat");
+        System.out.println("\tA: Yes");
+        System.out.println("\tB: No");
+        System.out.println("\tC: Not even top 5");
+        
+        System.out.println("Who has the most sosa");
+        System.out.println("\tA: Clarke");
+        System.out.println("\tB: Chief Keef");
+        System.out.println("\tC: Bobby Johnson");
+        
+        System.out.println("Hardest Soulja Boy song");
+        System.out.println("\tA: Crank That");
+        System.out.println("\tB: Turn my swag on");
+        System.out.println("\tC: Pretty Boy Swag");
+        
+        System.out.println("Minimum age to transport placarded amounts of Hazardous Materials?");
+        System.out.println("\tA: 18");
+        System.out.println("\tB: 16");
+        System.out.println("\tC: 21");
+        
+        System.out.println("Hardest Soulja Boy song");
+        System.out.println("\tA: Crank That");
+        System.out.println("\tB: Turn my swag on");
+        System.out.println("\tC: Pretty Boy Swag");
+    }
+}
